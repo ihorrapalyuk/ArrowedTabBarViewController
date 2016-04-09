2177d5e66bc8db3e584a5602f4600edd9aa0d5a1
Ihor Rapalyuk <i.rapalyuk2012@gmail.com>
2016-04-09 17:41:27 +0300

    Initial Commit

---
+++ ArrowedTabBarViewController/ArrowedTabBarViewController.h
@@ -0,0 +1,19 @@
+//
+//  ArrowedTabBarViewController.h
+//  ArrowedTabBarViewController
+//
+//  Created by Ihor Rapalyuk on 4/9/16.
+//  Copyright © 2016 Ihor Rapalyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArrowedTabBarViewController.
+FOUNDATION_EXPORT double ArrowedTabBarViewControllerVersionNumber;
+
+//! Project version string for ArrowedTabBarViewController.
+FOUNDATION_EXPORT const unsigned char ArrowedTabBarViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArrowedTabBarViewController/PublicHeader.h>
+
+
